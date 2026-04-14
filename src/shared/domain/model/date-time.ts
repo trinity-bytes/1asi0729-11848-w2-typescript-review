1deebea2910d58af24b83d5f479ec5092a0cf280
Jahat Trinidad <jahatjassiel@gmail.com>
2026-04-14 11:56:17 -0500

    feat(date-time): add DateTime and currency classes with formatting capabilities.

---
+++ src/shared/domain/model/date-time.ts
@@ -0,0 +1,46 @@
+/**
+ * Value object representing a date and time
+ * Ensures that the date is not in the future and is valid
+ * @remarks
+ * This class is inmutable and provides a method to format the date as a string
+ */
+export class DateTime {
+    private readonly _date: Date;
+
+    /**
+     * Creates a new DateTime instance
+     * @remarks
+     * Of no value is provided, the current date and time is used
+     * If
+     * @param value
+     */
+    constructor(value?: Date | String) {
+        const now = new Date();
+
+        if (value) {
+            const parseDate = new Date(value);
+
+            if (isNaN(parseDate.getTime()))
+                throw new Error("Invalid date: &{parseDate}");
+            if (parseDate > now)
+                throw new Error("Date cannot be in the future: &{parseDate}");
+        } else this._date = now;
+    }
+
+    /**
+     * Gets de underlying date object
+     * @return Thw
+     */
+    public get value():Date { return this._date; }
+
+    public format(locale:string = 'en-US'): string {
+        return this._date.toLocaleString(locale, {
+            year: 'numeric',
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit"
+        });
+    }
+}
